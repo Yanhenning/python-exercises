d9ba4586629d7fdaacb945bbc345231ec6167808
yanhenning <yanhenning@gmail.com>
2021-07-06 11:56:47 -0300

    fix the second question

--- two/second_exercise.py
+++ two/second_exercise.py
@@ -5,6 +5,10 @@ async def print_async_array(array):
     seconds = 1
     for i in range(len(array)):
         print(seconds)
-        seconds = seconds * 2
         await asyncio.sleep(seconds)
+        seconds = seconds * 2
         print(array[i])
+
+
+if __name__ == '__main__':
+    asyncio.run(print_async_array(['a', 'b', 'c', 'd']))
